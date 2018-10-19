a359c4b140fc1d8659d0e6340ba8b62d3a9e39bb
David Shorthouse <davidpshorthouse@gmail.com>
2018-10-19 09:28:07 -0400

    Strip out notifications

--- models/user.rb
+++ models/user.rb
@@ -125,45 +125,4 @@ class User < ActiveRecord::Base
     "(user_occurrences.action LIKE '%recorded%' OR user_occurrences.action LIKE '%identified%')"
   end
 
-  def check_changes
-    changes = []
-    visible_occurrences.each do |o|
-      response = gbif_response(o.gbifID)
-      if response[:scientificName] != o.scientificName
-        changes << { gbifID: o.gbifID, old_name: o.scientificName.dup, new_name: response[:scientificName] }
-        o.scientificName = response[:scientificName]
-      end
-      o.lastChecked = Time.now
-      o.save
-    end
-    notify(changes) if !changes.empty?
-  end
-
-  def notify(changes)
-    updates = changes.map{|c| "Old name: #{c[:old_name]}, New name: #{c[:new_name]}, See: https://www.gbif.org/occurrence/#{c[:gbifID]}"}.join("\n")
-    Pony.options = {
-      subject: "Bloodhound Notification: Your specimens have been re-identified",
-      body: "Hello #{given} #{family},\n\nAt least one of your specimens have been re-identified and are now being shared under a new scientific name.\n\n#{updates}",
-      via: :sendmail,
-      via_options: {
-        address:              Sinatra::Application.settings.smtp_address,
-        port:                 '587',
-        enable_starttls_auto: true,
-        user_name:            Sinatra::Application.settings.smtp_email,
-        password:             Sinatra::Application.settings.smtp_password,
-        authentication:      :login,
-        domain:              Sinatra::Application.settings.smtp_email.split("@").last
-      }
-    }
-    Pony.mail(to: email, from: Sinatra::Application.settings.smtp_email)
-  end
-
-  def gbif_response(occurrence_id)
-    response = RestClient::Request.execute(
-      method: :get,
-      url: Sinatra::Application.settings.gbif_api + 'occurrence/' + occurrence_id.to_s,
-    )
-    JSON.parse(response, :symbolize_names => true)
-  end
-
 end
