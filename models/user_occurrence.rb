7d9ae0c4e59ea796e6e7a4032ccb1942744c9f92
David Shorthouse <davidpshorthouse@gmail.com>
2018-10-19 22:08:51 -0400

    Add update hook to user_occurrences

--- models/user_occurrence.rb
+++ models/user_occurrence.rb
@@ -2,4 +2,14 @@ class UserOccurrence < ActiveRecord::Base
    belongs_to :occurrence
    belongs_to :user
    has_one :taxon_occurrence, foreign_key: :occurrence_id, primary_key: :occurrence_id
+
+   before_update :set_update_time
+
+   private
+
+   def set_update_time
+     self.updated = Time.now
+   end
+
+
 end
