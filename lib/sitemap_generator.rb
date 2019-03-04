2720cd0e8b2771c4dee083a7b4ebeba96e5968aa
David Shorthouse <davidpshorthouse@gmail.com>
2019-03-04 14:10:59 -0500

    Add more pages to sitemap

--- lib/sitemap_generator.rb
+++ lib/sitemap_generator.rb
@@ -13,6 +13,7 @@ module Bloodhound
     def add_flat_pages
       @map.add '/about'
       @map.add '/agents'
+      @map.add '/get-started'
       @map.add '/integrations'
       @map.add '/organizations'
       @map.add '/roster'
@@ -21,6 +22,14 @@ module Bloodhound
     def add_users
       User.where(is_public: true).find_each do |user|
         @map.add "/#{user.identifier}"
+        @map.add "/#{user.identifier}/specialties"
+        @map.add "/#{user.identifier}/co-collectors"
+        @map.add "/#{user.identifier}/identified-for"
+        @map.add "/#{user.identifier}/identifications-by"
+        @map.add "/#{user.identifier}/deposited-at"
+        @map.add "/#{user.identifier}/citations"
+        @map.add "/#{user.identifier}/specimens"
+        @map.add "/#{user.identifier}/comments"
       end
     end
 
