9cc0e54100e96c188317dbec30e5b7fe86b0a894
David Shorthouse <davidpshorthouse@gmail.com>
2019-04-20 01:12:04 -0400

    Fix bug in image upload

--- helpers/helpers.rb
+++ helpers/helpers.rb
@@ -64,7 +64,7 @@ module Sinatra
           if user.wikidata
             img =  cloud_img + user.image_url
           else
-            img = cloud_img + Sinatra::Application.settings.base_url + "/images/user/" + user.image_url
+            img = cloud_img + Sinatra::Application.settings.base_url + "/images/users/" + user.image_url
           end
         end
         img
