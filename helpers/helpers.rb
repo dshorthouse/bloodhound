d3017b4649c7638ac927c3f7a06fc4890bac167b
David Shorthouse <davidpshorthouse@gmail.com>
2019-06-05 23:44:37 -0400

    Put score back

--- helpers/helpers.rb
+++ helpers/helpers.rb
@@ -130,7 +130,7 @@ module Sinatra
               end
             end
           end
-          agents.delete_if{|a| !keepers.include?(a) }
+          agents.delete_if{|a| !keepers.include?(a) || a[:score] < 30 }
         end
         agents.compact.uniq.sort_by{|a| a[:score]}.reverse
       end
