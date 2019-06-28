a1bfe98055b00367b705f01b5c52a62197ec8c94
David Shorthouse <davidpshorthouse@gmail.com>
2019-06-28 16:17:20 -0400

    Speed-up taxon worker

--- lib/taxon_worker.rb
+++ lib/taxon_worker.rb
@@ -7,9 +7,12 @@ module Bloodhound
 
     def perform(row)
       taxon = Taxon.create_or_find_by(family: row["family"].strip)
-      occurrence_ids = row["gbifIDs_family"].tr('[]', '').split(',').map(&:to_i)
-      data = occurrence_ids.map{|r| { occurrence_id: r, taxon_id: taxon.id}}
-      TaxonOccurrence.import data, batch_size: 500, validate: false, on_duplicate_key_ignore: true
+      data = row["gbifIDs_family"].tr('[]', '')
+                        .split(',')
+                        .map{|r| { occurrence_id: r.to_i, taxon_id: taxon.id } }
+      if !data.empty?
+        TaxonOccurrence.import data, batch_size: 2500, validate: false, on_duplicate_key_ignore: true
+      end
     end
 
   end
