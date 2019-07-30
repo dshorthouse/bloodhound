8000322ebfa4fd005235d76cf18d80e156cd28cc
David Shorthouse <davidpshorthouse@gmail.com>
2019-07-30 15:26:59 -0400

    Add user's image to twitter card when present

--- helpers/formatters.rb
+++ helpers/formatters.rb
@@ -30,6 +30,8 @@ module Sinatra
           cloud_img = "https://abekpgaoen.cloudimg.io/crop/24x24/n/"
         elsif size == "medium"
           cloud_img = "https://abekpgaoen.cloudimg.io/crop/48x48/n/"
+        elsif size == "social"
+          cloud_img = "https://abekpgaoen.cloudimg.io/crop/240x240/n/"
         end
         if user.image_url
           if user.wikidata
