002797e99c2ed4c56f49e9f7c228ced9ff34f27c
David Shorthouse <davidpshorthouse@gmail.com>
2019-07-29 20:23:10 -0400

    More refinements to UI

--- public/js/application.js
+++ public/js/application.js
@@ -328,7 +328,7 @@ var Application = (function($, window) {
     helper_navbar: function() {
       var self = this;
       if ($('#helper-info').length && $('#helper-navbar').length) {
-        if (self.path === "/profile" || self.path === "/admin") {
+        if (self.path === "/help-others" || self.path === "/admin") {
           var navbar = $('#helper-navbar');
           $(document).scroll(function() {
             if ($(this).scrollTop() > $('#helper-info').offset().top) {
