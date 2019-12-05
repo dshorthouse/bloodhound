36df1a22978752f5aaa943d570d5686ded529f79
David Shorthouse <davidpshorthouse@gmail.com>
2019-12-04 21:09:54 -0500

    Use dwc_agent instead of Namae to parse wikidata names upon creation

--- lib/wikidata_search.rb
+++ lib/wikidata_search.rb
@@ -285,7 +285,7 @@ module Bloodhound
         return
       end
 
-      parsed = Namae.parse(wiki_user.title)[0] rescue nil
+      parsed = DwcAgent.parse(wiki_user.title)[0] rescue nil
 
       family = parsed.family rescue nil
       given = parsed.given rescue nil
