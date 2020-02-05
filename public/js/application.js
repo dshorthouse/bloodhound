b5daf4a0a684a7bf49637a20524a70fce3f0e01b
David Shorthouse <davidpshorthouse@gmail.com>
2020-02-05 09:30:00 -0500

    Minor fix to js

--- public/js/application.js
+++ public/js/application.js
@@ -449,7 +449,7 @@ var Application = (function($, window) {
       } else if (self.path === "/help-others" && self.identifier) {
         slug = self.path + "/" + self.identifier;
       }
-      if (slug) {
+      if (slug.length > 0) {
         $.ajax({
           method: "GET",
           url: slug + "/candidate-count.json"
