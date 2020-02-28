d3783e7b312312c0e69264bb38f533238fdeaca8
David Shorthouse <davidpshorthouse@gmail.com>
2020-02-28 16:02:38 -0500

    Add JSON for google GeoChart hierarchy

---
+++ public/js/geochart-hierarchy.js
@@ -0,0 +1,191 @@
+{
+  "world" : {
+    "id": 1,
+    "children": [
+      {
+        "name": "Africa",
+        "id": "002",
+        "children": [
+          {
+            "name": "Northern Africa",
+            "id": "015",
+            "children": [
+              "DZ", "EG", "EH", "LY", "MA", "SD", "SS", "TN"
+            ]
+          },
+          {
+            "name": "Western Africa",
+            "id": "011",
+            "children": [
+              "BF", "BJ", "CI", "CV", "GH", "GM", "GN", "GW", "LR", "ML", "MR", "NE", "NG", "SH", "SL", "SN", "TG"
+            ]
+          },
+          {
+            "name": "Middle Africa",
+            "id": "017",
+            "children": [
+              "AO", "CD", "ZR", "CF", "CG", "CM", "GA", "GQ", "ST", "TD"
+            ]
+          },
+          {
+            "name": "Eastern Africa",
+            "id": "014",
+            "children": [
+              "BI", "DJ", "ER", "ET", "KE", "KM", "MG", "MU", "MW", "MZ", "RE", "RW", "SC", "SO", "TZ", "UG", "YT", "ZM", "ZW"
+            ]
+          },
+          {
+            "name": "Southern Africa",
+            "id": "018",
+            "children": [
+              "BW", "LS", "NA", "SZ", "ZA"
+            ]
+          }
+        ]
+      },
+      {
+        "name": "Europe",
+        "id": "150",
+        "children": [
+          {
+            "name": "Northern Europe",
+            "id": "154",
+            "children": [
+              "GG", "JE", "AX", "DK", "EE", "FI", "FO", "GB", "IE", "IM", "IS", "LT", "LV", "NO", "SE", "SJ"
+            ]
+          },
+          {
+            "name": "Western Europe",
+            "id": "155",
+            "children": [
+              "AT", "BE", "CH", "DE", "DD", "FR", "FX", "LI", "LU", "MC", "NL"
+            ]
+          },
+          {
+            "name": "Eastern Europe",
+            "id": "151",
+            "children": [
+              "BG", "BY", "CZ", "HU", "MD", "PL", "RO", "RU", "SU", "SK", "UA"
+            ]
+          },
+          {
+            "name": "Southern Europe",
+            "id": "039",
+            "children": [
+              "AD", "AL", "BA", "ES", "GI", "GR", "HR", "IT", "ME", "MK", "MT", "RS", "PT", "SI", "SM", "VA", "YU"
+            ]
+          }
+        ]
+      },
+      {
+        "name": "Americas",
+        "id": "019",
+        "children": [
+          {
+            "name": "Northern America",
+            "id": "021",
+            "children": [
+              "BM", "CA", "GL", "PM","US"
+            ]
+          },
+          {
+            "name": "Caribbean",
+            "id": "029",
+            "children": [
+              "AG", "AI", "AN", "AW", "BB", "BL", "BS", "CU", "DM", "DO", "GD", "GP", "HT", "JM", "KN", "KY", "LC", "MF", "MQ", "MS", "PR", "TC", "TT", "VC", "VG", "VI"
+            ]
+          },
+          {
+            "name": "Central America",
+            "id": "013",
+            "children": [
+              "BZ", "CR", "GT", "HN", "MX", "NI", "PA", "SV"
+            ]
+          },
+          {
+            "name": "South America",
+            "id": "005",
+            "children": [
+              "AR", "BO", "BR", "CL", "CO", "EC", "FK", "GF", "GY", "PE", "PY", "SR", "UY", "VE"
+            ]
+          }
+        ]
+      },
+      {
+        "name": "Asia",
+        "id": "142",
+        "children": [
+          {
+            "name": "Central Asia",
+            "id": "143",
+            "children": [
+              "TM", "TJ", "KG", "KZ", "UZ"
+            ]
+          },
+          {
+            "name": "Eastern Asia",
+            "id": "030",
+            "children": [
+              "CN", "HK", "JP", "KP", "KR", "MN", "MO", "TW"
+            ]
+          },
+          {
+            "name": "Southern Asia",
+            "id": "034",
+            "children": [
+              "AF", "BD", "BT", "IN", "IR", "LK", "MV", "NP", "PK"
+            ]
+          },
+          {
+            "name": "South-Eastern Asia",
+            "id": "035",
+            "children": [
+              "BN", "ID", "KH", "LA", "MM", "BU", "MY", "PH", "SG", "TH", "TL", "TP", "VN"
+            ]
+          },
+          {
+            "name": "Western Asia",
+            "id": "145",
+            "children": [
+              "AE", "AM", "AZ", "BH", "CY", "GE", "IL", "IQ", "JO", "KW", "LB", "OM", "PS", "QA", "SA", "NT", "SY", "TR", "YE", "YD"
+            ]
+          }
+        ]
+      },
+      {
+        "name": "Oceania",
+        "id": "009",
+        "children": [
+          {
+            "name": "Australia and New Zealand",
+            "id": "053",
+            "children": [
+              "AU", "NF", "NZ"
+            ]
+          },
+          {
+            "name": "Melanesia",
+            "id": "054",
+            "children": [
+              "FJ", "NC", "PG", "SB", "VU"
+            ]
+          },
+          {
+            "name": "Micronesia",
+            "id": "057",
+            "children": [
+              "FM", "GU", "KI", "MH", "MP", "NR", "PW"
+            ]
+          },
+          {
+            "name": "Polynesia",
+            "id": "061",
+            "children": [
+              "AS", "CK", "NU", "PF", "PN", "TK", "TO", "TV", "WF", "WS"
+            ]
+          }
+        ]
+      }
+    ]
+  }
+}
